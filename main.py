1fb6dfe614c8596299fc43827364c732c960fb8a
Colin Eckert <colineckert10@gmail.com>
2025-04-06 16:23:28 -0700

    Add Line and Point classes, implement line drawing in Window class, and create .gitignore

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from window import Window
+from line import Line
+from point import Point
 
 def main():
     window = Window(800, 600)
+    point1 = Point(100, 100)
+    point2 = Point(200, 500)
+    point3 = Point(300, 300)
+    line = Line(point1, point2)
+    line2 = Line(point2, point3)
+    window.draw_line(line, "red")
+    window.draw_line(line2, "blue")
+
     window.wait_for_close()
 
 if __name__ == "__main__":
--- window.py
+++ window.py
@@ -21,3 +21,5 @@ class Window:
     def close(self):
         self.__running = False
             
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
